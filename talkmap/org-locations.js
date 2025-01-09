0afb0a61d980aa7bc1991a49ec5e27b9ece3de88
Dominik Schmidt <64283216+domidt@users.noreply.github.com>
2025-01-09 15:01:36 +0100

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -6,5 +6,25 @@ var addressPoints = [
   [
     "Dijon, France",
     47.32795874456784, 5.041112302338468
+  ],
+  [
+    "Lyon, France",
+    45.763420, 4.834277
+  ],
+  [
+    "Montpellier, France",
+    43.611900, 3.877200
+  ],
+  [
+    "Verona, Italý",
+    45.433334, 10.983333
+  ],
+  [
+    "Regensburg, Germany",
+    49.016667, 12.083333
+  ],
+  [
+    "Paris, France",
+    48.864716, 2.349014
   ]
 ];
